d22a6235553d25fbf1e6a3bdebade586218f1a91
IlinArtem78 <artem.ilin1994@gmail.com>
2024-11-18 17:17:12 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using EF_Library; 
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (var db = new EF_Library.AppContext())
+        {
+            var user1 = new User { Name = "Artur", Role = "admin" };
+            var user2 = new User { Name = "Sergey", Role = "user" }; 
+            db.Users.Add(user1);
+            db.Users.Add(user2);    
+            db.SaveChanges();
+        }
+
+    }
+
+
+}
