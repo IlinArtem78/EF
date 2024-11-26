b3eeea800d3bc5927157b49caec05c4160b5cd9d
IlinArtem78 <artem.ilin1994@gmail.com>
2024-11-26 11:31:30 +0300

    Работает только с английскими симфолами.

--- Program.cs
+++ Program.cs
@@ -1,48 +1,55 @@
 ﻿using EF_Library;
 using EF_Library.Models;
 using EF_Library.View;
-using Microsoft.Identity.Client;
 
 
 class Program
 {
+
+    public static List<User>? users { get; set; }
+    public static List<Book>? books { get; set; }
     static void Main(string[] args)
     {
 
-        Console.OutputEncoding = System.Text.Encoding.UTF8;
-        Console.WriteLine("Работа приложения практического задания PJ-02 <<<<ILIN.Artem>>>>");
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Работа приложения практического задания PJ-02 <<<<ILIN Artem>>>>");
         Console.WriteLine();
         Console.WriteLine();
-        Console.WriteLine("Выберите необходимую команду для работы с базой"); 
-        Console.WriteLine();
-        Console.WriteLine("1 - Ввести данные в базу; 2 - Работа моделью данных;  3 - Сортировка книг; 4 - Сортировка пользователей ");
-        byte num = byte.Parse(Console.ReadLine());
-        try {
-            var test = new UserAddBooks();
-            switch (num)
+        while (true)
         {
-            case 1:
-                        
-                break;
-                case 2:
+            Console.WriteLine("Выберите необходимую команду для работы с базой");
+            Console.WriteLine();
+            Console.WriteLine("1 - Добавление данных в модель; 2 - Работа c моделью данных;  3 - Сортировка");
+            byte num = byte.Parse(Console.ReadLine());
+            try
+            {
+               
+                switch (num)
+                {
+                    case 1:
+                        var inputData = new InputData();
+                        users = inputData.InputDataUsers(); 
+                        books = inputData.InputDataBooks();
+                        break;
+                    case 2:
+                        var workRepo = new WorkWithRepo(books,users);
+                        break;
+                    case 3:
+                        var sb = new SortedBooks(new BookRepository(), new UserRepository());
+                        break;
+                 
+                    default:
+                        Console.WriteLine();
+                        Console.WriteLine("Значение не верно, повторите ввод");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
 
-                break;
-                case 3:
-                    var sb = new SortedBooks(new BookRepository()); 
-                break;
-                case 4:
-
-                break;
-                default: Console.WriteLine();
-                Console.WriteLine("Значение не верно, повторите ввод"); 
-                break;
+            }
         }
-        }
-        catch (Exception ex) {
-            Console.WriteLine(ex.ToString());   
-
-        }
-
 
     }
 
